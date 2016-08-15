c738dd9a3cd82b886049b5ae5e3be98957d39289
Yury Kurnosov <y.kurnosov@gmail.com>
2016-08-16 03:54:50 +0400

    Create video-element-controller.meta.js

---
+++ video-element-controller.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         video-element-controller
+// @version      0.1
+// @namespace    https://github.com/Kurnosov/video-element-controller
+// @description  Adds useful keyboard shortcuts for HTML5 video.
+// @homepageURL  https://github.com/Kurnosov/video-element-controller
+// @updateURL    https://raw.github.com/Kurnosov/video-element-controller/master/video-element-controller.meta.js
+// @downloadURL  https://raw.github.com/Kurnosov/video-element-controller/master/video-element-controller.user.js
+// @run-at       document-start
+// @include      http*://*.youtube.com/*
+// @include      http*://*.gfycat.com/*
+// @include      http*://*.vimeo.com/*
+// @include      https://www.facebook.com/video.php*
+// @include      https://www.facebook.com/*/videos/*
+// @include      https://www.kickstarter.com/*
+// @include      http*://2ch.hk/*
+// @include      http*://arhivach.org/*
+// @include      *
+// @grant        GM_log
+// ==/UserScript==
